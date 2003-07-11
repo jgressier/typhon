44118d870506e59df232b7499209568b83fd8221
Jeremie Gressier <jeremie.gressier@isae.fr>
2003-07-11 08:23:08 +0000

    Mise � jour de commentaires

--- SOURCE/MODCOM/TYPHMAKE.f90
+++ SOURCE/MODCOM/TYPHMAKE.f90
@@ -1,4 +1,15 @@
- module TYPHMAKE
+!----------------------------------------------------------------------------------------
+! MODULE : TYPHMAKE                       Auteur : J. Gressier
+!                                         Date   : Octobre 2002
+! Fonction                                Modif  : 
+!   Variables globales du code TYPHON
+!   Definition de la précision machine et de tailles générales
+!
+! Defauts/Limitations/Divers :
+!
+!----------------------------------------------------------------------------------------
+
+module TYPHMAKE
 
   integer, parameter :: krp = 4  ! simple (4) ou double (8) precision
   !integer, parameter :: kip = 4  ! simple (4) ou double (8) precision
