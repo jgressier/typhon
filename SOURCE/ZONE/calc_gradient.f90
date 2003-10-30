a64e561881224b79c669f68615eaa096ba95d66f
Jeremie Gressier <jeremie.gressier@isae.fr>
2003-10-30 09:53:30 +0000

    Task 0003 : am�lioration calcul gradients (avec ghost-cell)

--- SOURCE/ZONE/calc_gradient.f90
+++ SOURCE/ZONE/calc_gradient.f90
@@ -37,7 +37,7 @@ real(krp)              :: imat(3,3)   ! matrice locale
 real(krp)              :: dsca        ! variation de variable scalaire
 type(v3d)              :: dvec        ! variation de variable vectorielle
 integer                :: ic, nc      ! indice et nombre de cellules internes
-integer                :: if, nf      ! indice et nombre de faces
+integer                :: if, nf, nfi ! indice et nombre de faces totales et internes
 integer                :: nv          ! nombre de variables
 integer                :: is, iv      ! indice de variable scalaire et vectorielle
 integer                :: ic1, ic2    ! indices de cellules (gauche et droite de face)
@@ -45,11 +45,13 @@ integer                :: info, xinfo ! retour d'info des routines LAPACK
 
 ! -- Debut de la procedure --
 
-nc = mesh%ncell_int   ! nombre de cellules internes
-nf = mesh%nface_int   ! nb de faces connectées avec 2 cellules
+nc  = mesh%ncell_int   ! nombre de cellules internes
+nfi = mesh%nface_int   ! nb de faces internes (connectées avec 2 cellules)
+nf  = mesh%nface       ! nb de faces totales 
 allocate(dcg(nf))
 
 ! -- Calcul des différences de centres de cellules --
+!    (toutes les faces, même limites, doivent avoir un centre de cellule)
 
 do if = 1, nf
   ic1 = mesh%facecell%fils(if,1)
@@ -64,7 +66,9 @@ do ic = 1, nc
   mat(ic)%mat = 0._krp
 enddo
 
-do if = 1, nf
+! -- boucle sur les faces internes uniquement (code source doublé)  --
+
+do if = 1, nfi
   imat(1,1) = dcg(if)%x **2
   imat(2,2) = dcg(if)%y **2
   imat(3,3) = dcg(if)%z **2
@@ -84,10 +88,30 @@ do if = 1, nf
   mat(ic2)%mat(:,:) = mat(ic2)%mat(:,:) + imat(:,:)
 enddo
 
-! Correction de la matrice dans les cas 2D (vecteur supplémentaire selon z)
+! -- boucle sur les faces limites uniquement (code source doublé) --
+
+do if = nfi+1, nf
+  imat(1,1) = dcg(if)%x **2
+  imat(2,2) = dcg(if)%y **2
+  imat(3,3) = dcg(if)%z **2
+  imat(1,2) = dcg(if)%x * dcg(if)%y
+  imat(2,1) = imat(1,2)
+  imat(1,3) = dcg(if)%x * dcg(if)%z
+  imat(3,1) = imat(1,3)
+  imat(2,3) = dcg(if)%y * dcg(if)%z
+  imat(3,2) = imat(2,3)
+  
+  ! contribution de la face à la cellule à gauche (UNIQUEMENT)
+  ic1 = mesh%facecell%fils(if,1)
+  mat(ic1)%mat(:,:) = mat(ic1)%mat(:,:) + imat(:,:)
+enddo
+
+! -- Correction de la matrice dans les cas 2D (vecteur supplémentaire selon z) --
+
 do ic = 1, nc
   mat(ic)%mat(3,3) = mat(ic)%mat(3,3) + 1._krp
 enddo
+
 
 
 ! l'inversion peut se faire des façon suivantes selon A symétrique (PO) ou non (GE)
@@ -100,9 +124,13 @@ do ic = 1, nc
   ! décomposition de Choleski
   call lapack_potrf('U', 3, mat(ic)%mat, 3, info)
   if (info /= 0) xinfo = ic
+  !if (info /= 0) then
+  !  xinfo = ic
+  !  print*,"DEBUG!!: xinfo = ",xinfo
+  !endif
 enddo
 
-!!print*,"DEBUG!!: xinfo = ",xinfo
+!print*,"DEBUG!!: xinfo = ",xinfo
 if (xinfo /= 0) call erreur("Routine LAPACK","Problème POTRF")
 
 ! -- Calcul du second membre -At.dT et résolution (multiplication par l'inverse) --
@@ -115,14 +143,22 @@ rhs(:,:) = 0._krp      ! initialisation
 ! calcul des gradients de scalaires
 
 do is = 1, gfield%nscal
-  ! Calcul des seconds membres
-  do if = 1, nf
+
+  ! Calcul des seconds membres (cellules internes et limites)
+  do if = 1, nfi
     ic1  = mesh%facecell%fils(if,1)
-    ic2  = mesh%facecell%fils(if,1)
+    ic2  = mesh%facecell%fils(if,2)
     dsca = gfield%tabscal(is)%scal(ic2) - gfield%tabscal(is)%scal(ic1) 
     rhs(1:3, ic1) = rhs(1:3, ic1) - dsca*tab(dcg(if))
     rhs(1:3, ic2) = rhs(1:3, ic2) + dsca*tab(dcg(if))
   enddo
+  do if = nfi+1, nf
+    ic1  = mesh%facecell%fils(if,1)
+    ic2  = mesh%facecell%fils(if,2)
+    dsca = gfield%tabscal(is)%scal(ic2) - gfield%tabscal(is)%scal(ic1) 
+    rhs(1:3, ic1) = rhs(1:3, ic1) - dsca*tab(dcg(if))
+  enddo
+
   ! Resolution
   xinfo = 0
   do ic = 1, nc
