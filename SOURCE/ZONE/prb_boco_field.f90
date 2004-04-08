8ac3696855eb4ea4365f2c33ab01ac05819bad7a
Jeremie Gressier <jeremie.gressier@isae.fr>
2004-04-08 08:55:15 +0000

    Task 8 : ajout de proc�dure

---
+++ SOURCE/ZONE/prb_boco_field.f90
@@ -0,0 +1,39 @@
+!------------------------------------------------------------------------------!
+! Procedure : prb_boco_field              Auteur : J. Gressier
+!                                         Date   : Novembre 2003
+! Fonction                                Modif  : (cf historique)
+!   Stockage de champ
+!
+! Defauts/Limitations/Divers :
+!
+!------------------------------------------------------------------------------!
+subroutine prb_boco_field(zone)
+
+use TYPHMAKE
+use OUTPUT
+use VARCOM
+use DEFZONE
+
+implicit none
+
+! -- Declaration des entrées --
+type(st_zone) :: zone            ! zone 
+
+! -- Declaration des sorties --
+
+! -- Declaration des variables internes --
+integer    :: ic                 ! index de capteur
+
+! -- Debut de la procedure --
+
+
+
+
+!-----------------------------
+endsubroutine prb_boco_field
+
+!------------------------------------------------------------------------------!
+! Historique des modifications
+!
+! nov 2003 : création
+!------------------------------------------------------------------------------!
